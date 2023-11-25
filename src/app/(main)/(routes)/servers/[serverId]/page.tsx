82f3a532e654d2a167fbc92d847b7739e6a339c9
Roman Paravaev <105551615+falkomerr@users.noreply.github.com>
2023-11-25 23:34:05 +0300

    bug fix

--- src/app/(main)/(routes)/servers/[serverId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,8 +1,45 @@
+import { currentProfile } from '@/utils/current-profile';
+import { db } from '@/utils/db';
+import { redirectToSignIn } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
 interface props {
-    serverId: string;
+    params: {
+        serverId: string;
+    };
 }
 
-const ServerIdPage = async ({ serverId }: props) => {
-    return <div>wdkwkd</div>;
+const ServerIdPage = async ({ params }: props) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirectToSignIn();
+    }
+
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: {
+                    profileId: profile.id,
+                },
+            },
+        },
+        include: {
+            channels: {
+                where: {
+                    name: 'general',
+                },
+            },
+        },
+    });
+
+    const generalChannel = server?.channels[0];
+
+    if (generalChannel?.name !== 'general') {
+        return null;
+    }
+
+    return redirect(`/servers/${server?.id}/channels/${generalChannel?.id}`);
 };
 export default ServerIdPage;
